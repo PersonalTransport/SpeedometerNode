d24192699d2d4a502c9bf4e699a74aeadd9958e8
Aaron Siegel <siegelaaron94@gmail.com>
2016-04-17 20:59:40 -0700

    removed the timer interrupt.

--- src/main.c
+++ src/main.c
@@ -2,8 +2,8 @@
 #pragma config DSWDTPS = DSWDTPSF // DSWDT Postscale Select (1:2,147,483,648 (25.7 days))
 #pragma config DSWDTOSC = LPRC // Deep Sleep Watchdog Timer Oscillator Select (DSWDT uses Low Power RC Oscillator (LPRC))
 #pragma config RTCOSC = SOSC // RTCC Reference Oscillator Select (RTCC uses Secondary Oscillator (SOSC))
-#pragma config DSBOREN = OFF // Deep Sleep BOR Enable bit (BOR disabled in Deep Sleep)
-#pragma config DSWDTEN = OFF // Deep Sleep Watchdog Timer (DSWDT disabled)
+#pragma config DSBOREN = ON // Deep Sleep BOR Enable bit (BOR enabled in Deep Sleep)
+#pragma config DSWDTEN = ON // Deep Sleep Watchdog Timer (DSWDT enabled)
 
 // CONFIG3
 #pragma config WPFP = WPFP63 // Write Protection Flash Page Segment Boundary (Highest Page (same as page 21))
@@ -16,12 +16,12 @@
 // CONFIG2
 #pragma config POSCMOD = HS // Primary Oscillator Select (HS Oscillator mode selected)
 #pragma config I2C1SEL = PRI // I2C1 Pin Select bit (Use default SCL1/SDA1 pins for I2C1 )
-#pragma config IOL1WAY = OFF // IOLOCK One-Way Set Enable (Once set, the IOLOCK bit cannot be cleared)
-#pragma config OSCIOFNC = OFF // OSCO Pin Configuration (OSCO pin functions as clock output (CLKO))
+#pragma config IOL1WAY = OFF // IOLOCK One-Way Set Enable (The IOLOCK bit can be set and cleared using the unlock sequence)
+#pragma config OSCIOFNC = ON // OSCO Pin Configuration (OSCO pin functions as port I/O (RA3))
 #pragma config FCKSM = CSDCMD // Clock Switching and Fail-Safe Clock Monitor (Sw Disabled, Mon Disabled)
-#pragma config FNOSC = FRCDIV // Initial Oscillator Select (Fast RC Oscillator with Postscaler (FRCDIV))
-#pragma config PLL96MHZ = OFF // 96MHz PLL Startup Select (96 MHz PLL Startup is enabled automatically on start-up)
-#pragma config PLLDIV = NODIV // USB 96 MHz PLL Prescaler Select (Oscillator input used directly (4 MHz input))
+#pragma config FNOSC = PRIPLL // Initial Oscillator Select (Primary Oscillator with PLL module (XTPLL, HSPLL, ECPLL))
+#pragma config PLL96MHZ = ON // 96MHz PLL Startup Select (96 MHz PLL Startup is enabled automatically on start-up)
+#pragma config PLLDIV = DIV3 // USB 96 MHz PLL Prescaler Select (Oscillator input divided by 3 (12 MHz input))
 #pragma config IESO = ON // Internal External Switchover (IESO mode (Two-Speed Start-up) enabled)
 
 // CONFIG1
@@ -55,8 +55,7 @@ int main()
 
     // Set UART TX to interrupt level 6
     // Set UART RX to interrupt level 6
-    // Set the timer used for break detection to interrupt level 7
-    struct l_irqmask irqmask = { 6, 6, 7 };
+    struct l_irqmask irqmask = { 6, 6 };
     l_sys_irq_restore(irqmask);
     
     l_bool configuration_ok = false;
@@ -86,11 +85,7 @@ void __attribute__((interrupt, no_auto_psv)) _U1TXInterrupt()
 {
     if (IFS0bits.U1TXIF) {
         IFS0bits.U1TXIF = 0;
-
         l_ifc_tx_UART1();
-
-        if (U1STAbits.FERR)
-            U1STAbits.FERR = 0;
     }
 }
 
@@ -98,11 +93,7 @@ void __attribute__((interrupt, no_auto_psv)) _U1RXInterrupt()
 {
     if (IFS0bits.U1RXIF) {
         IFS0bits.U1RXIF = 0;
-
         l_ifc_rx_UART1();
-
-        if (U1STAbits.FERR)
-            U1STAbits.FERR = 0;
     }
 }
 
@@ -110,10 +101,9 @@ struct l_irqmask l_sys_irq_disable()
 {
     IEC0bits.U1RXIE = 0;
     IEC0bits.U1TXIE = 0;
-    IEC0bits.IC1IE = 0;
     IFS0bits.U1TXIF = 0;
     IFS0bits.U1RXIF = 0;
-    struct l_irqmask mask = { IPC2bits.U1RXIP, IPC3bits.U1TXIP, IPC0bits.IC1IP };
+    struct l_irqmask mask = { IPC2bits.U1RXIP, IPC3bits.U1TXIP };
     return mask;
 }
 
@@ -126,8 +116,4 @@ void l_sys_irq_restore(struct l_irqmask previous)
     IPC3bits.U1TXIP = previous.tx_level;
     IFS0bits.U1RXIF = 0;
     IEC0bits.U1TXIE = 1;
-
-    IPC0bits.IC1IP = previous.t1_level;
-    IFS0bits.IC1IF = 0;
-    IEC0bits.IC1IE = 1;
 }
